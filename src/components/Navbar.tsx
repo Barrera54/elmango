90d645cd4bb92d52ae0e6411b078f9cb1425c9a2
Barrera54 <samuelbarrerapatino2403@gmail.com>
2025-05-03 17:57:58 -0500

    Se termino el diseño de la pagina primcipal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,6 +39,7 @@ const Navbar = ({ show }: { show: boolean }) => {
         </li>
       </ul>
     </div>
+    
   );
 }
 
--- src/pages/Catalogo.tsx
+++ src/pages/Catalogo.tsx
@@ -1,3 +1,4 @@
+
 const Ventas = () =>{
     return <div>Catalogo</div>
 }
