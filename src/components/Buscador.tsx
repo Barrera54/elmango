8ead865d8501dabb3a5a55aef020e20a438e0044
Barrera54 <samuelbarrerapatino2403@gmail.com>
2025-05-08 21:28:39 -0500

    SE creo la plantilla de inventario

--- src/components/Buscador.tsx
+++ src/components/Buscador.tsx
@@ -1,105 +1,37 @@
-import { useState } from 'react'
-import Navbar from './Navbar'
-import './css/App.css'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { BrowserRouter as Router } from 'react-router-dom'
-import DataTable, { TableColumn } from 'react-data-table-component'
+{/*
+RT04 Como desarrollador frontend quiero crear un apartado llamado inventario para que el usuario pueda agregar, editar y eliminar productos*/}
+import './css/buca.css'; // Importa el archivo de estilos
 
-
-
-interface ProductoRow {
-  Producto: string;
-  Precio: string; 
-  Cantidad: string; 
-  Hora?: string; 
-}
-
-function Appsito() {
-  const [showNav, setShowNav] = useState(false)
-
-  const columns: TableColumn<ProductoRow>[] = [
-    {
-      name: "Producto",
-      selector: (row: ProductoRow) => row.Producto,
-    },
-    {
-      name: "Cantidad",
-      selector: (row: ProductoRow) => row.Cantidad,
-    },
-    {
-      name: "Precio",
-      selector: (row: ProductoRow) => row.Precio,
-    },
-    {
-      name: "Hora",
-      selector: (row: ProductoRow) => row.Hora || "",
-    }
-  ];
-
-  const data: ProductoRow[] = [
-    { Producto: "Mango", Precio: "3000", Cantidad: "2" },
-    { Producto: "Leche", Precio: "12000", Cantidad: "3" },
-    { Producto: "Papa", Precio: "3000", Cantidad: "2" },
-    { Producto: "Cilantro", Precio: "1000", Cantidad: "1.5" },
-    { Producto: "Pasta", Precio: "4000", Cantidad: "1" },
-    { Producto: "Salsa roja", Precio: "2000", Cantidad: "1" },
-    { Producto: "Yogurt colanta de mora", Precio: "12000", Cantidad: "3" },
-    { Producto: "Trapeador", Precio: "6000", Cantidad: "1" },
-    { Producto: "Escoba", Precio: "5000", Cantidad: "1" },
-    { Producto: "Crema de Leche", Precio: "2500", Cantidad: "3" },
-    { Producto: "Lechuga", Precio: "2000", Cantidad: "1" },
-  ];
-
-  const customStyles = {
-    headCells: {
-      style: {
-        fontSize: '18px',
-        fontWeight: 'bold',
-      },
-    },
-    cells: {
-      style: {
-        fontSize: '16px',
-      },
-    },
-  };
-
+function Inventario() {
   return (
-    <>
-      <Router>
-        <header>
-          <GiHamburgerMenu onClick={() => setShowNav(!showNav)} />
-          <div className="a">El mango</div>
-        </header>
-        <Navbar show={showNav} />
-        <div className='main'>
-        </div>
-        
-      </Router>
-
-      <div className='el'>
-        <DataTable
-          columns={columns}
-          data={data}
-          customStyles={customStyles}
+    <div className="">
+   <div className="li"><h1>Inventario</h1></div>
+      <div className="search-container"> 
+      <div className="multicolor-search">
+        <input 
+          type="text" 
+          placeholder="Buscar..." 
+          className="search-input"
         />
+        <button className="search-button">
+          <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+          </svg>
+        </button>
       </div>
-
-      <div className="p">
-       <a href="./pages/venta"><button>Cliente</button></a>
-        <button>Facturas</button>
-        <button>Deudores</button>
-        <button>Domicilios</button>
-        <button>Anular</button>
-        <button>Pago</button>
+    </div>
+       <div className="pro">
+        <h1>Producto</h1>
+      </div><div className="linea-horizontal">
       </div>
-
-      <div className="la">
-        <h1>Total</h1>
-        <div className={`lo ${showNav ? 'hidden-line' : ''}`}></div>
+      <div className="linea-vertical"><div className="pi"></div><div className="botones-fila">
+        <button className="edit">Editar</button>
+        <button className="borro">Borrar</button>
+        <button className="agre">Agrega</button></div>
       </div>
-    </>
-  )
+      
+    </div>
+  );
 }
 
-export default Appsito
+export default Inventario;
