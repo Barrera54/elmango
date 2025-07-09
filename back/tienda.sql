79fd3ec41517cd6ecc3db44bb31f5bafaa3302ed
Barrera54 <128190035+Barrera54@users.noreply.github.com>
2025-07-08 21:13:43 -0500

    Delete back directory

--- back/tienda.sql
+++
@@ -1,8 +0,0 @@
-create database  IF NOT EXISTS tienda;
-use tienda;
-create table IF NOT EXISTS productos(
-    id int primary key auto_increment,
-    nombre varchar(100) not null,
-    descripcion text,
-    precio decimal(10,2) not null
-);
